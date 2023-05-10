ddb7c653b49f9287d0be96954bc724c6c631757a
marialuizalacerda <lacerdamarialuiza1@gmail.com>
2023-05-10 09:38:06 -0300

    continuação do jogo

--- sketch.js
+++ sketch.js
@@ -5,13 +5,12 @@ const ALTURA_JANELA = 400;
 // variaveis da bolinha
 let xBolinha = 300;
 let yBolinha = 200;
-let diametro = 15;
+let diametro = 20;
 let raio = diametro / 2 ;
 
 //velocidade da bolinha
 let velocidadeXBolinha = 6;
 let velocidadeYBolinha = 6;
-
 let raqueteComprimento = 10;
 let raqueteAltura = 90;
 
@@ -28,6 +27,11 @@ let velocidadeYOponente;
 let meusPontos = 0;
 let pontosDoOponente = 0;
 
+// chace do oponente errar
+let chanceDeErrar = 0;
+let direcaoRaqueteOponente = 1;
+
+
 function setup() {
   createCanvas(LARGURA_JANELA, ALTURA_JANELA);
 }
@@ -39,12 +43,16 @@ function draw() {
     verificaColisaoBorda(); // 4 - Verifica Colisão da bolinha
     mostraRaquete(xRaquete, yRaquete); // 5 - Desenha a raquete
     movimentaMinhaRaquete(); // 6 - Movimenta a raquete
-    verificaColisaoRaquete(); //
+
+    //verificaColisaoRaquete();
+    verificaColisaoRaquete(xRaquete, yRaquete); //
     mostraRaquete(xRaqueteOponente, yRaqueteOponente); //
     movimentaRaqueteOponente();
-    verificaColisaoRaqueteOponente();
+    verificaColisaoRaqueteOponente(xRaqueteOponente, yRaqueteOponente); //
     incluiPlacar();
     marcaPonto();
+    calculaChanceDeErrar(); 
+    bolinhaNaoBuga();
     
    
     // 7- Volta para o início da função draw()
@@ -109,15 +117,45 @@ function verificaColisaoRaqueteOponente() {
   }
   }
 
-function movimentaRaqueteOponente() {
-  velocidadeYOponente = yBolinha -yRaqueteOponente - raqueteComprimento / 2 - 30;
-  yRaqueteOponente += velocidadeYOponente
-}
+  function movimentaRaqueteOponente(){
+    const mediaYBolinha = yBolinha + raio;
+    const mediaYRaqueteOponente = yRaqueteOponente + (raqueteAltura/2);
+
+    if (mediaYBolinha > mediaYRaqueteOponente) {
+      direcaoRaqueteOponente = 1;
+    } else {
+      direcaoRaqueteOponente = -1;
+    }
+    yRaqueteOponente += 5 * random(0.55, 0.95) * direcaoRaqueteOponente;
+  }
+
+  function calculaChanceDeErrar() {
+    if (pontosDoOponente >= meusPontos) {
+      chanceDeErrar += 1
+      if (chanceDeErrar >= 39){
+      chanceDeErrar = 40
+      }
+    } else {
+      chanceDeErrar -= 1
+      if (chanceDeErrar <= 35){
+      chanceDeErrar = 35
+      }
+    }
+  }
+
 
 function incluiPlacar(){
-  fill(255)
-  text(meusPontos, 278, 26);
-  text(pontosDoOponente, 321, 26)
+  stroke(255);
+  textAlign(CENTER);  
+  textSize(16);
+  fill(color(255, 140, 0))
+  rect(150, 10, 40, 20);
+  fill(255);
+  text(meusPontos, 170, 26);
+  fill(color(255, 140, 0))
+  rect(450, 10, 40, 20);
+  fill(255);
+  text(pontosDoOponente, 470, 26)
 }
 
 function marcaPonto(){
@@ -128,6 +166,14 @@ function marcaPonto(){
   if (xBolinha <= raio){
     pontosDoOponente += 1;
   
+  }
+}
+
+function bolinhaNaoBuga(){
+  if (xBolinha - raio <= 0){
+    xBolinha = 20;
+  } else if (xBolinha >= 588){
+    xBolinha = width - 20;
   }
 }
 
